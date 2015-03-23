f9edeb9d35efef2749e1de11dbe5f5486c108d80
Gautham Warrier <gautham.r.w@gmail.com>
2015-03-23 16:54:57 +0530

    Fix overflow issues. Replace placeholder dictionary with proper one. Add CLI menu. Convert all to lowercase

--- src/com/spt/Main.java
+++ src/com/spt/Main.java
@@ -8,36 +8,78 @@ public class Main {
     private static HashMap<Integer, Stack<String>> dictionary = new HashMap<Integer, Stack<String>>();
 
     public static void main(String[] args) {
-	    Scanner in = new Scanner(System.in);
-        String enc = in.nextLine();
         try {
             ClassLoader classLoader = Main.class.getClassLoader();
             File file = new File(classLoader.getResource("dictionary.txt").getFile());
             String content = new Scanner(file).useDelimiter("\\Z").next();
             buildDictionary(content);
         } catch (Exception e) {
-            //Do Nothing
             System.out.print(e.toString());
         }
-        List<String> solutions = getDecryptions(enc);
-        for (String solution : solutions) {
-            System.out.println(solution);
+        Random rand = new Random();
+        Scanner in = new Scanner(System.in);
+        int option;
+        String enc, dec;
+        do {
+            System.out.println("Options\n===============\n1) Encrypt\n2) Decrypt\n3) Exit");
+            try {
+                option = Integer.parseInt(in.nextLine());
+            } catch (Exception e) {
+                option = 999;
+            }
+            switch (option) {
+                case 1:
+                    System.out.print("Input String : ");
+                    dec = in.nextLine().toLowerCase();
+                    int  n = rand.nextInt(25) + 1;
+                    enc = shiftString(dec, n);
+                    System.out.println("Encrypted String : '" + enc + "' with offset " + n);
+                    break;
+                case 2:
+                    System.out.print("Input String : ");
+                    enc = in.nextLine().toLowerCase();
+                    List<String> solutions = getDecryptions(enc);
+                    if (solutions.size() > 1) {
+                        System.out.print("Solutions :");
+                        int i = 1;
+                        for (String solution : solutions) {
+                            System.out.println(i++ + ") " + solution);
+                        }
+                    } else System.out.println("Solution : " + solutions.get(0));
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Invalid option. Try again.");
+                    break;
+            }
+        } while (option != 3);
+    }
+
+    private static String shiftString(String S, int offset) {
+        return shiftString(S, offset, 1);
+    }
+
+    private static String shiftString(String S, int offset, int add) {
+        StringBuilder X = new StringBuilder(S);
+        for (int i = 0; i < X.length(); ++i) {
+            char c = X.charAt(i);
+            if (c == ' ') continue;
+            c = (char)(c + add * offset);
+            if (!(c >= 'a' && c <= 'z')) {
+                c = (char)(c - 26);
+                if (!(c >= 'a' && c <= 'z')) c = (char)(c + 52);
+            }
+            X.setCharAt(i, c);
         }
+        return X.toString();
     }
 
     private static List<String> getDecryptions(String enc) {
         List<String> solutions = new ArrayList<String>();
         Set<Integer> offsets = getPossibleOffsets(enc);
         for (Integer offset : offsets) {
-            StringBuilder solution = new StringBuilder(enc);
-            for (int i = 0; i < solution.length(); ++i) {
-                char c = solution.charAt(i);
-                if (c == ' ') continue;
-                c = (char)(c - offset);
-                if (!(c >= 'a' && c <= 'z')) c = (char)(c - 26);
-                solution.setCharAt(i, c);
-            }
-            solutions.add(solution.toString());
+            solutions.add(shiftString(enc, offset, -1));
         }
         if (solutions.size() == 0) solutions.add("Decryption Failed!");
         return solutions;
@@ -57,26 +99,29 @@ public class Main {
     private static Set<Integer> getDecryptionOffsetsForWord(String e) {
         Set<Integer> possibleOffsets = new HashSet<Integer>();
         List<Integer> delta = getDeltaString(e);
-        Stack<String> possibleSolutions = getDecrpytedSolutionsForWord(delta);
+        Stack<String> possibleSolutions = getDecryptedSolutionsForWord(delta);
         for (String solution : possibleSolutions) {
-            possibleOffsets.add(e.charAt(0) - solution.charAt(0));
+            int offset = e.charAt(0) - solution.charAt(0);
+            offset = (offset < 0) ? offset + 26 : offset;
+            possibleOffsets.add(offset);
         }
         return possibleOffsets;
     }
 
-    private static Stack<String> getDecrpytedSolutionsForWord(List<Integer> delta) {
+    private static Stack<String> getDecryptedSolutionsForWord(List<Integer> delta) {
         Stack<String> decryptedSolutionsForWord = dictionary.get(delta.hashCode());
         return (decryptedSolutionsForWord == null) ? new Stack<String>() : decryptedSolutionsForWord;
     }
 
     private static void buildDictionary(String d) {
-        String[] words = d.split(",");
+        String[] words = d.split("\n");
         for (String word : words) {
+            word = word.toLowerCase();
             List<Integer> delta = getDeltaString(word);
             if (!dictionary.containsKey(delta.hashCode())) {
                 dictionary.put(delta.hashCode(), new Stack<String>());
             }
-            getDecrpytedSolutionsForWord(delta).push(word);
+            getDecryptedSolutionsForWord(delta).push(word);
         }
     }
 
